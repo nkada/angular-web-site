6e2aff73b762d1ef1acc6d40a436c674379a36f7
Srujan Kada <Srujan Kada>
2016-12-27 18:19:40 +0000

    After Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {RouterModule} from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { ServicesModule } from './services/services.module';
+import {AboutUsModule} from './about-us/about-us.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -16,17 +20,24 @@ import { ErrorComponent } from './error/error.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    AppointmentComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpModule,    
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'appointment', component: AppointmentComponent },
+      { path: 'contact-us', component: ContactUsComponent },
+      { path: 'services', loadChildren: 'services'}
       { path: '**', component: ErrorComponent }
     ]),
+    ServicesModule,
+    AboutUsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
