3137b89852e9dc8b06df70bf9e9e8e0b6f2044c0
Naveen Kada <Naveen Kada>
2017-02-04 22:50:36 +0000

    added authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { ServicesModule } from './services/services.module';
-import {AboutUsModule} from './about-us/about-us.module';
+import { AboutUsModule } from './about-us/about-us.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,6 +13,20 @@ import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { AppointmentComponent } from './appointment/appointment.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AlertComponent } from './alert/alert.component';
+
+import { AlertService } from './services/alert.service';
+import { AuthenticationService } from './services/authentication.service';
+import { UserService } from './services/user.service';
+import { AuthGuard } from './guards/auth-guard';
+
+import { fakeBackendProvider } from './helper/fake-backend';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
 
 @NgModule({
   declarations: [
@@ -22,24 +36,38 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     HomeComponent,
     ErrorComponent,
     AppointmentComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    AlertComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,    
+    HttpModule,
     RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
+      { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
       { path: 'appointment', component: AppointmentComponent },
       { path: 'contact-us', component: ContactUsComponent },
-      { path: 'services', loadChildren: 'services'},
+      { path: 'services', loadChildren: 'services' },
       { path: '**', component: ErrorComponent }
     ]),
     ServicesModule,
     AboutUsModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    // providers used to create fake backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../model/user';
+import { UserService } from '../services/user.service';
+
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  currentUser: User;
+  users: User[] = [];
 
-
-  constructor() { }
+  constructor(private userService: UserService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   ngOnInit() {
+    this.loadAllUsers();
   }
+
+  private loadAllUsers() {
+    this.userService.getAll().subscribe(users => { this.users = users; });
+  }
+
+  deleteUser(id: number) {
+    this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+  }
+
 
 }
